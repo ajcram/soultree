c40ddf9618eefcc012e88660001e6e2dad9a7403
AJ Cram <alfredc@15MBP-00972.local>
2013-11-16 09:19:31 -0800

    Empty project commit

---
+++ SoulTree/SoulTree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoulTree
+//
+//  Created by AJ Cram on 11/16/13.
+//  Copyright (c) 2013 AJ Cram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoulTree/SoulTree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoulTree
+//
+//  Created by AJ Cram on 11/16/13.
+//  Copyright (c) 2013 AJ Cram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
